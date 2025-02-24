86a2760ccdddcce6b1f6cb579c04722a87318f56
MAnas677320 <seotools180@gmail.com>
2025-02-24 22:50:03 +0500

    Delete feedback.js

--- feedback.js
+++
@@ -1,5 +0,0 @@
-const btn = document.querySelector(".button");
-const feedback = document.querySelector(".feedback-container");
-btn.addEventListener("click", () =>{
-    feedback.innerHTML = `<h1>Thanks for your feedback</h1>`;
-});
